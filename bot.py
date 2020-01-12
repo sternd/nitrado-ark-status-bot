1fc16a016f6a05a487022898257c047b2e30480a
sternd <sterndonald@gmail.com>
2020-01-12 14:32:48 -0800

    Uses rich embed for status message

--- bot.py
+++ bot.py
@@ -35,29 +35,48 @@ def handler(event, context):
 
         nitrapi_config = json.loads(NITRAPI_ACCOUNT_CONFIG)
 
-        output_array = []
+        embed = discord.Embed(title="Valkyrie Server Status", colour=discord.Colour(0xf8e71c), url="https://github.com/sternd/nitrado-ark-status-bot", description="A visualization of the availability status for Valkyrie's Ark Nitrado servers. The status of all servers will be updated every minute.")
+        embed.set_thumbnail(
+            url="https://cdn.discordapp.com/icons/626094990984216586/ceb7d3a814435bc9601276d07f44b9f3.png?size=128")
+        embed.set_footer(text="Updated",
+                         icon_url="https://cdn.discordapp.com/icons/626094990984216586/ceb7d3a814435bc9601276d07f44b9f3.png?size=128")
 
         for account in nitrapi_config["nitrado_accounts"]:
             auth_token = account["auth_token"]
 
+            availability_status = 'available'
+
             for gameserver in account["gameservers"]:
-                if (gameserver["enabled"] != True):
+                if gameserver["enabled"] != True:
                     continue
 
                 gameserver_details = getGameserverDetails(auth_token, gameserver["gameserver_id"])
-                output_array.append(formatGameserverMessage(gameserver["gameserver_name"], gameserver_details))
 
-        formatted_status_message = '\n__**Valkyrie Server Status**__\n\n'
+                status = parseGameserverStatus(gameserver_details)
 
-        for gameserver_output in output_array:
-            formatted_status_message += gameserver_output + '\n'
+                if availability_status == 'down':
+                    availability_status = availability_status
+                elif status != 'started' and status != 'restarting':
+                    availability_status = 'down'
+                elif status == 'restarting':
+                    availability_status = 'restarting'
 
-        formatted_status_message += 'Last Updated: ' + datetime.utcnow().strftime("%H:%M:%S") + ' UTC'
+                embed = addRichEmbedField(embed, gameserver["gameserver_name"], status, gameserver_details)
+
+            if availability_status == 'available':
+                colour = discord.Colour(0x7ed321)
+            if availability_status == 'restarting':
+                colour = discord.Colour(0xf5a623)
+            if availability_status == 'down':
+                colour = discord.Color(0xD0021B)
+
+        embed.__setattr__('timestamp', datetime.utcnow())
+        embed.__setattr__('colour', colour)
 
         if status_message == None:
-            await channel.send(formatted_status_message)
+            await channel.send(embed=embed)
         else:
-            await status_message.edit(content=formatted_status_message)
+            await status_message.edit(embed=embed)
 
         await client.close()
 
@@ -65,18 +84,18 @@ def handler(event, context):
     # Return: Dict | None
     # See for more info: https://doc.nitrado.net/#api-Gameserver-Details
     def getGameserverDetails(token, id):
-        print(f"Getting gameserver details for {id}")
+        # print(f"Getting gameserver details for {id}")
         url = NITRAPI_BASE_URL + NITRAPI_GAMESERVER_DETAILS.replace(':id', id, 1)
         auth_token = f'Bearer {token}'
         try:
             response = requests.get(url, timeout=3, headers={"Authorization": auth_token})
             response.raise_for_status()
         except HTTPError as http_err:
-            print(f'HTTP error occurred: {http_err}')
+            print(f'HTTP error occurred for {id}: {http_err}')
         except Timeout as timeout:
-            print(f'HTTP timeout occurred: {timeout}')
+            print(f'HTTP timeout occurred for {id}: {timeout}')
         except Exception as err:
-            print(f'Other error occurred: {err}')
+            print(f'Other error occurred for {id}: {err}')
         else:
             return response.json()
 
@@ -99,13 +118,13 @@ def handler(event, context):
     def formatGameserverStatus(gameserver_status):
         if gameserver_status == "started":
             green_circle = discord.utils.get(client.emojis, name="green_circle")
-            return '🟢 Available'
+            return '🟢'
         elif gameserver_status == 'stopped' or gameserver_status == 'stopping' or gameserver_status == 'suspended':
-            return '🔴 Down'
+            return '🔴'
         elif gameserver_status == 'restarting':
-            return '🟠 Restarting'
+            return '🟠'
         else:
-            return '🔴 Unknown'
+            return '🔴'
 
     # Extract the gameserver player count information from the gameserver details API response
     # Return: Dict | None
@@ -113,25 +132,27 @@ def handler(event, context):
         try:
             players = {"current_players": gameserver_details['data']['gameserver']['query']['player_current'], "max_players": gameserver_details['data']['gameserver']['query']['player_max']}
         except Exception as err:
-            print(f'Error occurred: {err}')
+            # print(f'Error occurred: {err}')
+            return None
         else:
             return players
 
         return None
 
-    # Formats the message for an individual gameserver for output to Discord
-    # Return: String
-    def formatGameserverMessage(server_name, server_details):
-        status = parseGameserverStatus(server_details)
+    # Formats the message for an individual gameserver and adds it to an existing embed
+    # Return: Discord.Embed
+    def addRichEmbedField(embed, server_name, status, server_details):
         players = parseGameserverPlayers(server_details)
         formatted_status = formatGameserverStatus(status)
 
-        formatted_server_message = f'**{server_name}**\n*Status:*  {formatted_status}\n'
+        formatted_server_message = f'*Status:*  {formatted_status}\n'
 
         if players != None:
-            formatted_server_message += f'*Players:*  {players["current_players"]}/{players["max_players"]}\n'
+            formatted_server_message += f'*Players:*  {players["current_players"]}/{players["max_players"]}'
 
-        return formatted_server_message
+        embed.add_field(name='**' + server_name + '**', value=formatted_server_message, inline=True)
+
+        return embed
 
     # Starts the Discord Client connection
     client.run(TOKEN)
